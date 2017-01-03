5a9b58d5dbb89bb0adf89254cafb3303d2a28850
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2017-01-03 17:15:23 +0900

    jsp 부분 수정

--- src/main/java/bitcamp/java89/ems2/servlet/HeaderServlet.java
+++ src/main/java/bitcamp/java89/ems2/servlet/HeaderServlet.java
@@ -1,9 +1,9 @@
 package bitcamp.java89.ems2.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,33 +25,13 @@ public class HeaderServlet extends HttpServlet {
       throws ServletException, IOException {
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    
-    out.println("<div id='header' style='background-color:gray; height:40px;"
-        + "position:relative;'>");
-    
-    out.println("<div style='width: 300px; height:38px;"
-        + "position:absolute; left:0px; top:0px;'>");
-    out.println("<img src='../image/hoho.jpg' "
-        + " height='30' style='float:left; margin-top:6px; margin-left:7px;'>"); 
-    out.println("<div style='color:white; font-weight:bold; "
-        + " margin-left:60px; padding-top:3px; font-family: 고딕체, sans-serif; "
-        + " font-size:x-large;'>교육센터관리시스템</div>");
-    out.println("</div>");
-    
-    // 로그인 사용자 정보를 가져온다.
-    out.println("<div style='width:200px; height:30px; margin-top:5px; float:right;'>");
     Member member = (Member)request.getSession().getAttribute("member");
-    if (member == null) {
-      out.println("<a href='../auth/login'>로그인</a>");
-    } else {
-      // 업데이트부분-사진넣기
-      out.printf("<img src='../upload/%s' height='30' style='vertical-align:middle;'>", this.getPhotoPath(member));
-      out.printf("<span >%s</span>\n", member.getName());
-      out.println("<a href='../auth/logout'>로그아웃</a>");
+    
+    RequestDispatcher rd = request.getRequestDispatcher("/header.jsp");
+    if (member != null) {
+      request.setAttribute("photoPath", this.getPhotoPath(member));
     }
-    out.println("</div>");
-    out.println("</div>");
+    rd.include(request, response);
   }
   
   private String getPhotoPath(Member member) {
