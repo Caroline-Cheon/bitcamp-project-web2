4850cc8122cbc4bf5afc29c1c02dc2fc52041c18
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2017-01-18 17:11:40 +0900

    .

---
+++ src/main/java/bitcamp/java89/ems2/control/advice/WebControllerAdvice.java
@@ -0,0 +1,20 @@
+package bitcamp.java89.ems2.control.advice;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class WebControllerAdvice {
+  SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+  
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {   // 일관성을 주기위해서 메서드이름을 바인더라고 지정한 것뿐
+    System.out.println("MyBindingInitializer.initBinder()...");
+    dateFormat.setLenient(false);
+    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+  }
+}
