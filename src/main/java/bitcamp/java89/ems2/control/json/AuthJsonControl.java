081732d012441796193eee7ef135a0e478b3e73b
Caroline-Cheon <caroline.jy.cheon@gamil.com>
2017-02-01 13:01:35 +0900

    2017/02/01

--- src/main/java/bitcamp/java89/ems2/control/json/AuthJsonControl.java
+++ src/main/java/bitcamp/java89/ems2/control/json/AuthJsonControl.java
@@ -18,22 +18,10 @@ public class AuthJsonControl {
 
   
   @RequestMapping("/auth/login")
-  public AjaxResult login(String email, String password, String saveEmail, String userType, 
+  public AjaxResult login(String email, String password, String userType, 
       HttpServletResponse response, HttpSession session, Model model) throws Exception {
     
-    if (saveEmail != null) {
-      // 쿠키를 웹 브라우저에게 보낸다.
-      Cookie cookie = new Cookie("email", email);
-      cookie.setMaxAge(60 * 60 * 24 * 15);
-      response.addCookie(cookie);
-      
-    } else {
-      // 기존에 보낸 쿠키를 제거하라고 웹 브라우저에게 응답한다.
-      Cookie cookie = new Cookie("email", "");
-      cookie.setMaxAge(0);
-      response.addCookie(cookie);
-    }
-    //http://localhost:8080/bitcamp-project-web2/auth/login.json?email=user01@test.com&password=1111&userType=student&saveEmail=true
+    //http://localhost:8080/bitcamp-project-web2/auth/login.json?email=user01@test.com&password=1111&userType=student
     Member member = authService.getmemberInfo(email, password, userType);
     
     if (member == null) { // 멤버정보가 없으면 로그인 실패이다.
@@ -44,17 +32,10 @@ public class AuthJsonControl {
     return new AjaxResult(AjaxResult.SUCCESS, "로그인 성공!");
   }
   
-  @RequestMapping("/auth/loginform")
-  public String loginForm(Model model) throws Exception {
-    model.addAttribute("title", "로그인");
-    model.addAttribute("contentPage", "auth/loginform.jsp");
-    return "main";
-  }
-  
   @RequestMapping("/auth/logout")
-  public String logout(HttpSession session) throws Exception {
+  public AjaxResult logout(HttpSession session) throws Exception {
     session.invalidate();
-    return "redirect:loginform.do";
+    return new AjaxResult(AjaxResult.SUCCESS, "로그아웃 성공입니다.");
   }
   
   @RequestMapping("/auth/loginUser")
--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,17 +1,24 @@
 window.addEventListener("load", function() {
-// header.html을 가져와서 붙인다.
+	// header.html을 가져와서 붙인다.
 	get('../header.html', function(result) {
 	  //서버에서 로그인 사용자 정보를 가져온다.
 	  get('../auth/loginUser.json', function(jsonText) {
 		  var ajaxResult = JSON.parse(jsonText); // 파싱
 		  
-		  document.querySelector('#header').innerHTML = result;
+		  document.querySelector('#header').innerHTML = result; // 바깥 함수의 로컬변수를 사용하는 순간 복제를 통해 자신의 영역에 복제해논다. --> '클로저'
 		  // 화면에 뿌려지는 퍼포먼스를 고려하기위해서는 (로그인, 로그아웃이 동시에 나타나는 상태의 시간을 줄이기 위해서)
 		  // 최대한 화면에 늦게 출력한다. 그리고 바로 불필요한 창을 감춘다.
 		  
 		  if (ajaxResult.status == "fail") { // 로그인 되지 않았으면,
 			  // 로그온 상태 출력 창을 감춘다.
 			  document.querySelector('#logon-div').style.display = 'none';
+			  
+			  // 로그인버튼의 클릭 이벤트 핸들러 등록하기
+			  document.querySelector('#login-btn').onclick = function(event) {
+				  event.preventDefault()
+				  //alert("로그인")
+				  location.href = '../auth/main.html'
+			  };
 			  return;
 		  }
 		  
@@ -21,15 +28,25 @@ window.addEventListener("load", function() {
 			  '../upload/' + ajaxResult.data.photoPath;
 		  document.querySelector('#logon-div span').textContent = 
 			  ajaxResult.data.name;
+		  
+		  // 로그아웃 버튼의 클릭 이벤트 핸들러 등록하기
+		  document.querySelector('#logout-btn').onclick = function(event) {
+			  event.preventDefault();
+			  //alert("로그아웃");
+			  // 로그아웃은 로그아웃을 시킨다음에 입력폼으로 보낸다. 
+			  get('../auth/logout.json', function(jsonText) {
+				  location.href = '../auth/main.html'
+			  });
+		  };
 	  })
 	});
 	
-// sidebar.html을 가져와서 붙인다.
+	// sidebar.html을 가져와서 붙인다.
 	get('../sidebar.html', function(result) {
 		document.querySelector('#sidebar').innerHTML = result;
 	});
 	
-// footer.html을 가져와서 붙인다.
+	// footer.html을 가져와서 붙인다.
 	get('../footer.html', function(result) {
 		document.querySelector('#footer').innerHTML = result;
 	});
