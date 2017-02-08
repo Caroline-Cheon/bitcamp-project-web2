b985952b8f9b2b6f13fc18b11f0d9d77085782cc
CAROLINE <CAROLINE@DESKTOP-3R3CKAR>
2017-02-08 16:00:20 +0900

    ## 3.7 - handlebars HTML 템플릿 엔진 적용

--- src/main/webapp/student/main.js
+++ src/main/webapp/student/main.js
@@ -1,9 +1,5 @@
 //학생 목록 가져와서 tr 태그를 만들어 붙인다.
 $.getJSON('list.json', function(ajaxResult) {
-	/* @RestController 페이지 컨트롤러의 메서드가 리턴한 값은
-	 * AjaxResult의 프로퍼티 값으로 JSON 문자열을 만든다.
-	 * 따라서 status나 data 프로퍼티를 바로 꺼내면 된다.
-	 */
   var status = ajaxResult.status;
   
   if (status != "success")
@@ -12,22 +8,16 @@ $.getJSON('list.json', function(ajaxResult) {
   var list = ajaxResult.data;
   var tbody = $('#list-table > tbody');
   
-  for (var student of list) {
-    $("<tr>")
-      .html("<td>" + 
-	      student.memberNo + "</td><td><a class='name-link' href='#' data-no='" + 
-	      student.memberNo + "'>" + 
-	   	  student.name + "</a></td><td>" + 
-	      student.tel + "</td><td>" + 
-	      student.working + "</td><td>" +
-	      student.grade + "</td><td>" +
-	      student.schoolName + "</td>")
-	  .appendTo(tbody);
-  }
-	// 학생 목록에서 이름 링크에 click 이벤트를 처리한다.
-	$('.name-link').click(function(event) {
-		event.preventDefault(); // a 동작이 안되게 해야 한다.
-		location.href = 'view.html?memberNo=' + $(this).attr('data-no'); 
+  // 템플릿 텍스트를 처리하여 HTML을 생성해 줄 함수 얻기
+  var template = Handlebars.compile($('#trTemplate').html());
+  
+  // 템플릿 엔진을 통해 생성된 HTML을 tbody에 넣는다.
+  tbody.html(template({"list": list}));
+  
+  // 학생 목록에서 이름 링크에 click 이벤트를 처리한다.
+  $('.name-link').click(function(event) {
+	  event.preventDefault(); // a 동작이 안되게 해야 한다.
+	  location.href = 'view.html?memberNo=' + $(this).attr('data-no'); 
 	});
 });
 
